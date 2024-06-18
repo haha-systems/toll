bd51595cdeb4b95db0a2f1bd5e6e2dcb6c6ffc1e
Mark Anthony Gibbins <xiy3x0@gmail.com>
2024-06-18 23:46:27 +0100

    add support for  and gunicorn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import tiktoken
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,5 @@ def token_count():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
